30e484bbc5664466fcc8ad146ddd61f313daa136
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2014-11-23 13:50:55 +0100

    In setup.py, first attempt at writing custom commands. Will be used to parse header files and extract constants (cont'd).

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
+from distutils.core import Command
+
 from setuptools import setup
+
+class parse_header(Command):
+    user_options = [('where=', None, 'Path to the FreeImage.h header file.')]
+
+    def initialize_options(self):
+        print('Initializing options...')
+        self.where = None
+
+    def finalize_options(self):
+        print('Finalizing options...')
+        print(self.where)
+
+    def run(self):
+        print('running parse_header')
 
 setup(name='pyfreeimage',
       version='0.0',
       description='ctypes wrapper around the FreeImage library',
       author='Sébastien Brisard',
-      packages=['pyfreeimage'])
+      packages=['pyfreeimage'],
+      cmdclass={'parse_header': parse_header})
