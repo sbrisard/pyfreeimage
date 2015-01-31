b3a5ab837d68da298e992483d136a8292555ae47
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-01-31 07:22:00 +0100

    Command parse_header in setup.py now creates a dict of image formats.

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
+import itertools
+import re
+
+from collections import OrderedDict
 from distutils.core import Command
 
 from setuptools import setup
+
+def dict_to_str(d, indent=0):
+    prefix = ''.join(itertools.repeat(' ', indent))
+    it = iter(d)
+    key = next(it)
+    lines = ['{{{0}: {1}'.format(key, d[key])]
+    pattern = prefix + ' {0}: {1}'
+    for key in it:
+        lines.append(pattern.format(key, d[key]))
+    lines[-1] = lines[-1] + '}'
+    return '\n'.join(lines)
 
 class parse_header(Command):
     user_options = [('where=', None, 'Path to the FreeImage.h header file.')]
@@ -14,7 +29,24 @@ class parse_header(Command):
         print(self.where)
 
     def run(self):
-        print('running parse_header')
+        pattern = re.compile('\s*(FIF_\S+)\s*=\s*([^,\n]*)')
+        image_format = OrderedDict()
+
+        with open(self.where, 'r', encoding='latin-1') as f:
+            for line in f:
+                result = pattern.match(line)
+                if result is not None:
+                    key = result.group(1)
+                    try:
+                        value = int(result.group(2))
+                    except ValueError:
+                        value = image_format[result.group(2)]
+                    image_format[key] = value
+
+        with open('./pyfreeimage/constants.py', 'w') as f:
+            f.write('image_format = ' + dict_to_str(image_format,
+                                                    indent=15) + '\n')
+
 
 setup(name='pyfreeimage',
       version='0.0',
