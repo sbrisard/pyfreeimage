c26f4e262ebcdcb902519670bff7a672a1723795
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-02-04 05:58:31 +0100

    ICommand parse_header now creates enums.

--- setup.py
+++ setup.py
@@ -6,46 +6,50 @@ from distutils.core import Command
 
 from setuptools import setup
 
-def dict_to_str(d, indent=0):
-    prefix = ''.join(itertools.repeat(' ', indent))
-    it = iter(d)
-    key = next(it)
-    lines = ['{{{0}: {1}'.format(key, d[key])]
-    pattern = prefix + ' {0}: {1}'
-    for key in it:
-        lines.append(pattern.format(key, d[key]))
-    lines[-1] = lines[-1] + '}'
-    return '\n'.join(lines)
 
 class parse_header(Command):
     user_options = [('where=', None, 'Path to the FreeImage.h header file.')]
 
     def initialize_options(self):
-        print('Initializing options...')
         self.where = None
 
     def finalize_options(self):
-        print('Finalizing options...')
-        print(self.where)
+        pass
 
     def run(self):
-        pattern = re.compile('\s*(FIF_\S+)\s*=\s*([^,\n]*)')
-        image_format = OrderedDict()
+        enum_names = {'FIF': 'Format', 'FIT': 'Type'}
+        enum_members = self.parse_enums(enum_names.keys())
+        with open('./pyfreeimage/constants.py', 'w') as f:
+            self.write_enums(f, enum_names, enum_members)
+
+
+    def parse_enums(self, prefixes):
+        dicts = dict((prefix, OrderedDict()) for prefix in prefixes)
+        all_constants = dict()
+        pattern = re.compile('\s*((' +
+                             '|'.join(prefixes) +
+                             ')_(\S+))\s*=\s*([^,\n\s]*)')
 
         with open(self.where, 'r', encoding='latin-1') as f:
             for line in f:
                 result = pattern.match(line)
                 if result is not None:
-                    key = result.group(1)
+                    key = result.group(3).lower()
                     try:
-                        value = int(result.group(2))
+                        value = int(result.group(4))
                     except ValueError:
-                        value = image_format[result.group(2)]
-                    image_format[key] = value
+                        value = all_constants[result.group(4)]
+                    all_constants[result.group(1)] = value
+                    dicts[result.group(2)][key] = value
+        return dicts
 
-        with open('./pyfreeimage/constants.py', 'w') as f:
-            f.write('image_format = ' + dict_to_str(image_format,
-                                                    indent=15) + '\n')
+    def write_enums(self, f, enum_names, enum_members):
+        f.write('from enum import Enum\n')
+        for prefix, enum_name in enum_names.items():
+            f.write('\n\n')
+            f.write('class {0}(Enum):\n'.format(enum_name))
+            for item in enum_members[prefix].items():
+                f.write('    {0} = {1}\n'.format(*item))
 
 
 setup(name='pyfreeimage',
