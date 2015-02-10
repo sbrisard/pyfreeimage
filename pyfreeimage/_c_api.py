90694ad0122ed3b1e3ccc4cadef485cb00325dc0
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-02-10 05:29:58 +0100

    New module `_c_api` is the basic `ctypes` wrapper.

---
+++ pyfreeimage/_c_api.py
@@ -0,0 +1,29 @@
+import ctypes
+
+from ctypes import c_char_p
+from ctypes import c_int
+from ctypes import c_uint
+from ctypes import c_void_p
+
+libfi = ctypes.cdll.LoadLibrary('/opt/local/lib/libfreeimage.dylib')
+
+def init_signature(func_name, restype=c_uint, argtypes=[c_void_p]):
+    global libfi
+    f = getattr(libfi, 'FreeImage_' + func_name)
+    f.restype = restype
+    f.argtypes = argtypes
+
+init_signature('GetVersion', c_char_p, None)
+init_signature('GetCopyrightMessage', c_char_p, None)
+init_signature('GetFileType', c_int, [c_char_p, c_int])
+init_signature('AllocateT', c_void_p, [c_int, # type
+                                       c_int, c_int, # width, depth
+                                       c_int, # bpp
+                                       c_uint, c_uint, c_uint]) # RGB masks
+init_signature('GetImageType', c_int)
+init_signature('GetColorsUsed')
+init_signature('GetBPP')
+init_signature('GetWidth')
+init_signature('GetHeight')
+init_signature('GetLine')
+init_signature('Load', c_void_p, [c_int, c_char_p, c_int])
--- pyfreeimage/pyfreeimage.py
+++ pyfreeimage/pyfreeimage.py
@@ -1,23 +1,9 @@
-import ctypes
-
-from ctypes import c_char_p
-from ctypes import c_int
-from ctypes import c_uint
-from ctypes import c_void_p
-
 from .constants import Format
 from .constants import Type
-
-libfi = ctypes.cdll.LoadLibrary('/opt/local/lib/libfreeimage.dylib')
+from pyfreeimage._c_api import libfi
 
 _reverse_format = dict((f.value, f) for f in Format)
 _reverse_type = dict((t.value, t) for t in Type)
-
-def init_signature(func_name, restype=c_uint, argtypes=[c_void_p]):
-    global libfi
-    f = getattr(libfi, 'FreeImage_' + func_name)
-    f.restype = restype
-    f.argtypes = argtypes
 
 def get_version():
     """Return the current version of the library, as a string."""
@@ -61,18 +47,3 @@ def load(name, fmt=None, flags=0):
         fif = fmt.value
     dib = libfi.FreeImage_Load(fif, name, flags)
     return Bitmap(dib)
-
-init_signature('GetVersion', c_char_p, None)
-init_signature('GetCopyrightMessage', c_char_p, None)
-init_signature('GetFileType', c_int, [c_char_p, c_int])
-init_signature('AllocateT', c_void_p, [c_int, # type
-                                       c_int, c_int, # width, depth
-                                       c_int, # bpp
-                                       c_uint, c_uint, c_uint]) # RGB masks
-init_signature('GetImageType', c_int)
-init_signature('GetColorsUsed')
-init_signature('GetBPP')
-init_signature('GetWidth')
-init_signature('GetHeight')
-init_signature('GetLine')
-init_signature('Load', c_void_p, [c_int, c_char_p, c_int])
