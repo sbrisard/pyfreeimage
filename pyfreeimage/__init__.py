bfc5c484eb13c30d773ea4ec91d624295636a257
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-03-08 14:54:27 +0100

    Corrected some imports.

--- pyfreeimage/__init__.py
+++ pyfreeimage/__init__.py
@@ -1,4 +1,4 @@
 from pyfreeimage.bitmap import Bitmap, empty, load
-from pyfreeimage.bitmap import get_version, get_copyright_message, get_file_type
+from pyfreeimage.bitmap import get_file_type
 from pyfreeimage._constants import Format, Type
 from pyfreeimage._c_api import __copyright_message__, __version__
