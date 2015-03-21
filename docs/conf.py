0d8d9c8cfdec644b4c5e711835650fea5a429c9d
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-03-21 15:44:48 +0100

    Use RTD theme locally.

--- docs/conf.py
+++ docs/conf.py
@@ -137,7 +137,7 @@ todo_include_todos = True
 if on_rtd:
     html_theme = 'default'
 else:
-    html_theme = 'alabaster'
+    html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
