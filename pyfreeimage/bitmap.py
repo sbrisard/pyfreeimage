dde2a35b4ec03a2958c4bd7f239f90af3fed36e8
Sebastien Brisard <sebastien.brisard@ifsttar.fr>
2015-03-26 05:43:40 +0100

    Docstrings.

--- pyfreeimage/bitmap.py
+++ pyfreeimage/bitmap.py
@@ -19,33 +19,19 @@ class Bitmap:
         self.bpp = libfi.FreeImage_GetBPP(dib)
         finalize(self, libfi.FreeImage_Unload, dib)
 
-    def save(self, filename, fif, flags=0):
-        """Save the image to a file.
-
-        Args:
-            filename (str): File name.
-            fif (pyfreeimage.Format): File format.
-            flags (int): "or" combination of constants from the
-                ``pyfreeimage.ioflags`` module.
-
-        """
-        if isinstance(filename, str):
-            filename = filename.encode()
-        libfi.FreeImage_Save(fif.value, self._dib, filename, flags)
-
     def copy(self):
         """Return a copy of the image."""
         return Bitmap(libfi.FreeImage_Clone(self._dib))
 
 
 def get_file_type(filename, size=0):
-    """Return the `Type` of the image.
+    """Return the type of the image.
 
     Args:
-        filename (str): File name.
+        filename (str): Name of the file where the image is stored.
         size (int): unused.
     Returns:
-        The type of the image as an int.
+        The type of the image as an int (one of the ``FIF_*`` constants).
     """
     return libfi.FreeImage_GetFileType(filename, size)
 
@@ -58,9 +44,31 @@ def empty(width, height, bpp, rmask=0, gmask=0, bmask=0,
 
 
 def load(filename, fif=None, flags=0):
+    """Read an image from a file.
+
+    Args:
+        filename (str): Name of the file from which the image is read.
+        fif (int): File format; one of the ``FIF_*`` constants.
+        flags (int): or combination of the I/O flags.
+    """
     if isinstance(filename, str):
         filename = filename.encode()
     if fif is None:
         fif = libfi.FreeImage_GetFileType(filename, 0)
     dib=libfi.FreeImage_Load(fif, filename, flags)
     return Bitmap(dib)
+
+
+def save(filename, fif, flags=0):
+    """Save the image to a file.
+
+    Args:
+        filename (str): Name of the file to which the image is saved.
+        image (:class:`Bitmap`): Image to be saved.
+        fif (int): File format; one of the ``FIF_*`` constants.
+        flags (int): or combination of the I/O flags.
+
+    """
+    if isinstance(filename, str):
+        filename = filename.encode()
+    libfi.FreeImage_Save(fif.value, self._dib, filename, flags)
