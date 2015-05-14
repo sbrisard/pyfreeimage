f44a162c224304429b037f09352b5b93fade3e42
Sébastien Brisard <sbrisard@pc6.home>
2015-05-14 11:27:00 +0200

    Simplified metadata.Tag.value().

--- pyfreeimage/metadata.py
+++ pyfreeimage/metadata.py
@@ -9,13 +9,16 @@ from pyfreeimage.constants import *
 type_map = {FIDT_BYTE: ctypes.c_uint8,
             FIDT_SHORT: ctypes.c_uint16,
             FIDT_LONG: ctypes.c_uint32,
+            FIDT_RATIONAL: ctypes.c_uint32,
             FIDT_SBYTE: ctypes.c_int8,
             FIDT_UNDEFINED: ctypes.c_byte,
             FIDT_SSHORT: ctypes.c_int16,
             FIDT_SLONG: ctypes.c_int32,
+            FIDT_SRATIONAL: ctypes.c_int32,
             FIDT_FLOAT: ctypes.c_float,
             FIDT_DOUBLE: ctypes.c_double,
             FIDT_IFD: ctypes.c_uint32,
+            FIDT_PALETTE: ctypes.c_uint8,
             FIDT_LONG8: ctypes.c_uint64,
             FIDT_SLONG8: ctypes.c_int64,
             FIDT_IFD8: ctypes.c_uint64}
@@ -61,17 +64,18 @@ class Tag:
     def value(self):
         tag_type = self.type
         tag_value = cfi.FreeImage_GetTagValue(self._tag)
-        try:
-            return_type = type_map[tag_type]
-            if self.count > 1:
-                return_type = return_type*self.count
-            return return_type.from_address(tag_value)
-        except KeyError:
-            if tag_type == FIDT_ASCII:
-                return ctypes.cast(tag_value, c_char_p).value
-            elif tag_type == FIDT_RATIONAL:
-                return_type = ctypes.c_uint32*(2*self.count)
-                return return_type.from_address(tag_value)
-            else:
-                raise NotImplementedError('Cannot compute value of '
-                                          'tag type {}'.format(tag_type))
+
+        # If FIDT_ASCII, return a string
+        if tag_type == FIDT_ASCII:
+            return ctypes.cast(tag_value, c_char_p).value
+
+        # Return a ctypes array in all other cases
+        length = self.count
+        if (tag_type == FIDT_RATIONAL) or (tag_type == FIDT_SRATIONAL):
+            length *= 2
+        elif (tag_type == FIDT_PALETTE):
+            length *= 4
+        return_type = type_map[tag_type]
+        if length > 1:
+            return_type = return_type*length
+        return return_type.from_address(tag_value)
