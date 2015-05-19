18370276e5b918d1136f51b8c4a1271e9bf80d05
Sébastien Brisard <sbrisard@pc6.home>
2015-05-19 06:06:02 +0200

    Renamed Tag._tag → Tag._c_tag.

--- pyfreeimage/metadata.py
+++ pyfreeimage/metadata.py
@@ -27,38 +27,38 @@ class Tag:
     """TODO"""
 
     def __init__(self, ptag=None):
-        self._tag = ptag
-        #finalize(self, cfi.FreeImage_DeleteTag, self._tag)
+        self._c_tag = ptag
+        #finalize(self, cfi.FreeImage_DeleteTag, self._c_tag)
 
     def copy(self):
         """Return a deep copy of the tag."""
-        return Tag(cfi.FreeImage_CloneTag(self._tag))
+        return Tag(cfi.FreeImage_CloneTag(self._c_tag))
 
     @property
     def key(self):
         """The tag field name (unique inside a metadata model)."""
-        return cfi.FreeImage_GetTagKey(self._tag)
+        return cfi.FreeImage_GetTagKey(self._c_tag)
 
     @property
     def description(self):
         """The tag description."""
-        return cfi.FreeImage_GetTagDescription(self._tag)
+        return cfi.FreeImage_GetTagDescription(self._c_tag)
 
     @property
     def id(self):
         """The tag ID."""
-        return cfi.FreeImage_GetTagID(self._tag)
+        return cfi.FreeImage_GetTagID(self._c_tag)
 
     @property
     def type(self):
         """The data type."""
-        return cfi.FreeImage_GetTagType(self._tag)
+        return cfi.FreeImage_GetTagType(self._c_tag)
 
     @property
     def length(self):
         """The length of the array returned by :func:`Tag.value`."""
-        tag_type = cfi.FreeImage_GetTagType(self._tag)
-        tag_count = cfi.FreeImage_GetTagCount(self._tag)
+        tag_type = cfi.FreeImage_GetTagType(self._c_tag)
+        tag_count = cfi.FreeImage_GetTagCount(self._c_tag)
         if (tag_type == FIDT_RATIONAL) or (tag_type == FIDT_SRATIONAL):
             return 2*tag_count
         elif (tag_type == FIDT_PALETTE):
@@ -70,7 +70,7 @@ class Tag:
     def value(self):
         """TODO"""
         tag_type = self.type
-        tag_value = cfi.FreeImage_GetTagValue(self._tag)
+        tag_value = cfi.FreeImage_GetTagValue(self._c_tag)
 
         # If FIDT_ASCII, return a string
         if tag_type == FIDT_ASCII:
