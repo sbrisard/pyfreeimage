a23b872c49a723bbf19e851d8b9c6c1f250d1e7a
Sébastien Brisard <sbrisard@pc6.home>
2015-05-30 14:19:40 +0200

    Added URL for new test images.

--- tests/tstutil.py
+++ tests/tstutil.py
@@ -5,7 +5,9 @@ import pyfreeimage.io
 
 IMG_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images')
 
-IMG_URL = {'5.2.10.tiff': 'http://sipi.usc.edu/database/download.php?vol=misc&img=5.2.10'}
+IMG_URL = {'5.2.10.tiff': 'http://sipi.usc.edu/database/download.php?vol=misc&img=5.2.10',
+           'kodim22.png': 'http://r0k.us/graphics/kodak/kodak/kodim22.png',
+           'kochtreffen.jpg': 'http://upload.wikimedia.org/wikipedia/commons/thumb/3/36/13-09-01-kochtreffen-wien-RalfR-02.jpg/320px-13-09-01-kochtreffen-wien-RalfR-02.jpg'}
 
 def load_image(name):
     path = os.path.join(IMG_DIR, name)
