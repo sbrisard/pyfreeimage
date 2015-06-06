a568d89ebf8bce5e8df74a215757a7998af77979
Sébastien Brisard <sebastien.brisard@m4x.org>
2015-06-06 17:09:53 +0200

    No longer import get_file_format.

--- pyfreeimage/__init__.py
+++ pyfreeimage/__init__.py
@@ -10,7 +10,6 @@ Attributes:
 import pyfreeimage.io
 
 from pyfreeimage.image import Image, empty
-from pyfreeimage.io import get_file_format
 from pyfreeimage.constants import *
 from pyfreeimage._c_api import cfi
 
