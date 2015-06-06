657c48eb4886ce7792c80ace684a321e1168a82e
Sébastien Brisard <sebastien.brisard@m4x.org>
2015-06-06 07:22:03 +0200

    init_signature no longer has default args.

--- pyfreeimage/_c_api.py
+++ pyfreeimage/_c_api.py
@@ -11,7 +11,7 @@ if path_to_cfi is not None:
 else:
     raise RuntimeError('Could not locate the FreeImage shared library.')
 
-def init_signature(func_name, restype=c_uint, argtypes=[c_void_p]):
+def init_signature(func_name, restype, argtypes):
     global cfi
     f = getattr(cfi, func_name)
     f.restype = restype
@@ -22,28 +22,28 @@ init_signature('FreeImage_GetCopyrightMessage', c_char_p, None)
 init_signature('FreeImage_GetFileType', c_int, [c_char_p, c_int])
 init_signature('FreeImage_AllocateT', c_void_p,
                [c_int, c_int, c_int, c_int, c_uint, c_uint, c_uint])
-init_signature('FreeImage_GetImageType', c_int)
-init_signature('FreeImage_GetColorsUsed')
-init_signature('FreeImage_GetBPP')
-init_signature('FreeImage_GetWidth')
-init_signature('FreeImage_GetHeight')
-init_signature('FreeImage_GetLine')
-init_signature('FreeImage_GetPitch')
+init_signature('FreeImage_GetImageType', c_int, [c_void_p])
+init_signature('FreeImage_GetColorsUsed', c_uint, [c_void_p])
+init_signature('FreeImage_GetBPP', c_uint, [c_void_p])
+init_signature('FreeImage_GetWidth', c_uint, [c_void_p])
+init_signature('FreeImage_GetHeight', c_uint, [c_void_p])
+init_signature('FreeImage_GetLine', c_uint, [c_void_p])
+init_signature('FreeImage_GetPitch', c_uint, [c_void_p])
 init_signature('FreeImage_Load', c_void_p, [c_int, c_char_p, c_int])
 init_signature('FreeImage_Save', c_int, [c_int, c_void_p, c_char_p, c_int])
 init_signature('FreeImage_Clone', c_void_p, [c_void_p])
 init_signature('FreeImage_Unload', None, [c_void_p])
 
-init_signature('FreeImage_CreateTag', c_void_p, [])
-init_signature('FreeImage_DeleteTag', None)
-init_signature('FreeImage_CloneTag', c_void_p)
-init_signature('FreeImage_GetTagKey', c_char_p)
-init_signature('FreeImage_GetTagDescription', c_char_p)
-init_signature('FreeImage_GetTagID', c_uint16)
-init_signature('FreeImage_GetTagType', c_int)
-init_signature('FreeImage_GetTagCount', c_uint32)
-init_signature('FreeImage_GetTagLength', c_uint32)
-init_signature('FreeImage_GetTagValue', c_void_p)
+init_signature('FreeImage_CreateTag', c_void_p, None)
+init_signature('FreeImage_DeleteTag', None, [c_void_p])
+init_signature('FreeImage_CloneTag', c_void_p, [c_void_p])
+init_signature('FreeImage_GetTagKey', c_char_p, [c_void_p])
+init_signature('FreeImage_GetTagDescription', c_char_p, [c_void_p])
+init_signature('FreeImage_GetTagID', c_uint16, [c_void_p])
+init_signature('FreeImage_GetTagType', c_int, [c_void_p])
+init_signature('FreeImage_GetTagCount', c_uint32, [c_void_p])
+init_signature('FreeImage_GetTagLength', c_uint32, [c_void_p])
+init_signature('FreeImage_GetTagValue', c_void_p, [c_void_p])
 
 init_signature('FreeImage_FindFirstMetadata', c_void_p,
                [c_int, c_void_p, c_void_p_p])
